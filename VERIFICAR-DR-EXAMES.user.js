0541af81741a8aea6f33e854efa6fbccea03db0f
Nicolas <34519753+Nicker2@users.noreply.github.com>
2025-10-08 19:53:21 -0300

    Update VERIFICAR-DR-EXAMES.user.js

--- VERIFICAR-DR-EXAMES.user.js
+++ VERIFICAR-DR-EXAMES.user.js
@@ -5,7 +5,8 @@
 // @description Conta pacientes DR. EXAMES com logs detalhados, exibe apenas a lista superior por padrão, oculta a lista inferior até que a superior esteja fora de vista, nomes como hyperlinks azuis sem sublinhado, sem alterar a tabela #listaespera > tbody, adiciona botão para alternar visibilidade das listas apenas quando há pacientes, destaca "Primeira vez" com badge, verifica mensagem de login na página de login, adiciona botão com especialidade (verde para Oftalmologia, vermelho para outras) ao lado do nome do profissional na tabela e abaixo do nome no dropdown, remove o elemento ai-assistant-plugin e mantém o valor 30 no select de itens por página.
 // @author Você
 // @match https://*.feegow.com/*/*
-// @downloadURL https://raw.githubusercontent.com/Nicker2/Verificar-DR.EXAMES/refs/heads/main/VERIFICAR-DR-EXAMES.js
+// @downloadURL https://raw.githubusercontent.com/Nicker2/Verificar-DR.EXAMES/refs/heads/main/VERIFICAR-DR-EXAMES.user.js
+// @updateURL https://raw.githubusercontent.com/Nicker2/Verificar-DR.EXAMES/refs/heads/main/VERIFICAR-DR-EXAMES.meta.js
 // @grant none
 // ==/UserScript==
 
