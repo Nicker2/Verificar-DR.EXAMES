b4cd8bc67ef499eb0319b892628bc17db3fcc180
Nicolas <34519753+Nicker2@users.noreply.github.com>
2025-10-08 19:52:11 -0300

    Create VERIFICAR-DR-EXAMES.meta.js

---
+++ VERIFICAR-DR-EXAMES.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Contar DR. EXAMES com Logs Detalhados (Comparação Correta)
+// @namespace    http://tampermonkey.net/
+// @version      4.2
+// @description  Conta pacientes DR. EXAMES com logs detalhados, exibe duas listas idênticas (acima e abaixo do tbody) com nomes como hyperlinks azuis sem sublinhado, sem alterar a tabela #listaespera > tbody, adiciona botão para alternar visibilidade das listas apenas quando há pacientes, e destaca "Primeira vez" com badge.
+// @author       Você
+// @match        https://*.feegow.com/*/*
+// @downloadURL  https://raw.githubusercontent.com/Nicker2/Verificar-DR.EXAMES/refs/heads/main/VERIFICAR-DR-EXAMES.user.js
+// @updateURL    https://raw.githubusercontent.com/Nicker2/Verificar-DR.EXAMES/refs/heads/main/VERIFICAR-DR-EXAMES.meta.js
+// @icon         https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://app.feegow.com/&size=16
+// @grant         none
+// ==/UserScript==
