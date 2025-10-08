aadedd1f9720ea7d0f52529135f9b0548a67c576
Nicolas <34519753+Nicker2@users.noreply.github.com>
2025-10-08 19:53:34 -0300

    Update VERIFICAR-DR-EXAMES.meta.js

--- VERIFICAR-DR-EXAMES.meta.js
+++ VERIFICAR-DR-EXAMES.meta.js
@@ -1,12 +1,11 @@
 // ==UserScript==
-// @name         Contar DR. EXAMES com Logs Detalhados (Comparação Correta)
-// @namespace    http://tampermonkey.net/
-// @version      4.2
-// @description  Conta pacientes DR. EXAMES com logs detalhados, exibe duas listas idênticas (acima e abaixo do tbody) com nomes como hyperlinks azuis sem sublinhado, sem alterar a tabela #listaespera > tbody, adiciona botão para alternar visibilidade das listas apenas quando há pacientes, e destaca "Primeira vez" com badge.
-// @author       Você
-// @match        https://*.feegow.com/*/*
-// @downloadURL  https://raw.githubusercontent.com/Nicker2/Verificar-DR.EXAMES/refs/heads/main/VERIFICAR-DR-EXAMES.user.js
-// @updateURL    https://raw.githubusercontent.com/Nicker2/Verificar-DR.EXAMES/refs/heads/main/VERIFICAR-DR-EXAMES.meta.js
-// @icon         https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://app.feegow.com/&size=16
-// @grant         none
+// @name Contar DR. EXAMES com Logs Detalhados e Manter Valor 30 no Select
+// @namespace http://tampermonkey.net/
+// @version 4.9
+// @description Conta pacientes DR. EXAMES com logs detalhados, exibe apenas a lista superior por padrão, oculta a lista inferior até que a superior esteja fora de vista, nomes como hyperlinks azuis sem sublinhado, sem alterar a tabela #listaespera > tbody, adiciona botão para alternar visibilidade das listas apenas quando há pacientes, destaca "Primeira vez" com badge, verifica mensagem de login na página de login, adiciona botão com especialidade (verde para Oftalmologia, vermelho para outras) ao lado do nome do profissional na tabela e abaixo do nome no dropdown, remove o elemento ai-assistant-plugin e mantém o valor 30 no select de itens por página.
+// @author Você
+// @match https://*.feegow.com/*/*
+// @downloadURL https://raw.githubusercontent.com/Nicker2/Verificar-DR.EXAMES/refs/heads/main/VERIFICAR-DR-EXAMES.user.js
+// @updateURL https://raw.githubusercontent.com/Nicker2/Verificar-DR.EXAMES/refs/heads/main/VERIFICAR-DR-EXAMES.meta.js
+// @grant none
 // ==/UserScript==
