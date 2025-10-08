7522f29333b4e4f0e5d76afb115d6f79bb3ee088
Nicolas <34519753+Nicker2@users.noreply.github.com>
2025-10-08 20:02:42 -0300

    Update VERIFICAR-DR-EXAMES.user.js

--- VERIFICAR-DR-EXAMES.user.js
+++ VERIFICAR-DR-EXAMES.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Contar DR. EXAMES com Logs Detalhados e Manter Valor 30 no Select
-// @namespace https://github.com/Nicker2/Verificar-DR.EXAMES/
+// @namespace https://github.com/Nicker2/Verificar-DR.EXAMES
 // @version 4.9
 // @description Conta pacientes DR. EXAMES com logs detalhados, exibe apenas a lista superior por padrão, oculta a lista inferior até que a superior esteja fora de vista, nomes como hyperlinks azuis sem sublinhado, sem alterar a tabela #listaespera > tbody, adiciona botão para alternar visibilidade das listas apenas quando há pacientes, destaca "Primeira vez" com badge, verifica mensagem de login na página de login, adiciona botão com especialidade (verde para Oftalmologia, vermelho para outras) ao lado do nome do profissional na tabela e abaixo do nome no dropdown, remove o elemento ai-assistant-plugin e mantém o valor 30 no select de itens por página.
 // @author Você
