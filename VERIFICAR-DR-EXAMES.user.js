cd26ead462a8918bc636eb9224372aeb94402a8e
Nicolas <34519753+Nicker2@users.noreply.github.com>
2025-10-28 09:16:18 -0300

    novo dr
    novo dr

--- VERIFICAR-DR-EXAMES.user.js
+++ VERIFICAR-DR-EXAMES.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Contar DR. EXAMES com Logs Detalhados e Manter Valor 30 no Select
 // @namespace https://github.com/Nicker2/Verificar-DR.EXAMES
-// @version 4.9
+// @version 4.9.1
 // @description Conta pacientes DR. EXAMES com logs detalhados, exibe apenas a lista superior por padrão, oculta a lista inferior até que a superior esteja fora de vista, nomes como hyperlinks azuis sem sublinhado, sem alterar a tabela #listaespera > tbody, adiciona botão para alternar visibilidade das listas apenas quando há pacientes, destaca "Primeira vez" com badge, verifica mensagem de login na página de login, adiciona botão com especialidade (verde para Oftalmologia, vermelho para outras) ao lado do nome do profissional na tabela e abaixo do nome no dropdown, remove o elemento ai-assistant-plugin e mantém o valor 30 no select de itens por página.
 // @author Você
 // @match https://*.feegow.com/*/*
@@ -36,6 +36,7 @@
         "IAGO RAFAEL BRITO GUIMARAES": "Clínica Geral",
         "ISRAEL EMILIANO PACHECO": "Oftalmologia",
         "JOSE ERNESTO GHEDIN SERVIDEI": "Oftalmologia",
+        "LEONARDO DE ANGELLI BENEDITO CARDOSO": "Oftalmologia",
         "LEONEL TELLES DE MENEZES MORAIS": "Oftalmologia",
         "LEONNE DI CARLO DEL VECCHIO": "Dermatologia",
         "LINDA MARIA AVELAR MEDEIROS": "Dermatologia",
