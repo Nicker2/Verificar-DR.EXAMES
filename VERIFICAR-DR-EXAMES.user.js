7bc443acbe6e6bf1921c967fb671e07ef3052614
Nicolas <34519753+Nicker2@users.noreply.github.com>
2026-02-27 09:06:51 -0300

    Update VERIFICAR-DR-EXAMES.user.js

--- VERIFICAR-DR-EXAMES.user.js
+++ VERIFICAR-DR-EXAMES.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Contar DR. EXAMES com Logs Detalhados e Manter Valor 30 no Select
 // @namespace https://github.com/Nicker2/Verificar-DR.EXAMES
-// @version 4.9.2.0
+// @version 4.9.2.1
 // @description Conta pacientes DR. EXAMES com logs detalhados, exibe apenas a lista superior por padrão, oculta a lista inferior até que a superior esteja fora de vista, nomes como hyperlinks azuis sem sublinhado, adiciona botão para alternar visibilidade, destaca "Primeira vez" com badge, intercepta dados de login e faz Bypass Invisível de sessão dupla via Fetch API com tela de carregamento, adiciona especialidade e mantém valor 30.
 // @author Você
 // @match https://*.feegow.com/*/*
@@ -40,6 +40,7 @@
         "IAGO RAFAEL BRITO GUIMARAES": "Clínica Geral",
         "ISRAEL EMILIANO PACHECO": "Oftalmologia",
         "JOSE ERNESTO GHEDIN SERVIDEI": "Oftalmologia",
+        "JOÃO VICTOR DE ALMEIDA WESTPHAL": "Oftalmologia",
         "LEONARDO DE ANGELLI BENEDITO CARDOSO": "Oftalmologia",
         "LEONEL TELLES DE MENEZES MORAIS": "Oftalmologia",
         "LEONNE DI CARLO DEL VECCHIO": "Dermatologia",
@@ -48,6 +49,7 @@
         "LUIZA AMARANTE RODRIGUES": "Dermatologia",
         "MARCOS AURELIO COSTA": "Oftalmologia",
         "MARIO MONTINGELLI JUNIOR": "Cirurgia Vascular",
+        "MARIA CAROLINA RODRIGUES MARTINI": "Cardiologia"
         "NIXON LOPES DE ALMEIDA": "Oftalmologia",
         "PAULA RABELO HALFELD MENDONÇA": "Oftalmologia",
         "RAPHAEL GHEDIN SERVIDEI SANTANA": "Oftalmologia",
