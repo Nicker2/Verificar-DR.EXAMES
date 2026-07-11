0a2dc87ea1d6646160aa4d418e054b380ec001f9
Nicolas <34519753+Nicker2@users.noreply.github.com>
2026-07-11 06:27:44 -0300

    Create core.meta.js

---
+++ core.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Suite Feegow Enhanced Core Injector Agent
+// @namespace    https://github.com/Nicker2
+// @version      1.0
+// @description  Puxa o código fonte em tempo real do GitHub.
+// @author Nicolas Bonza Cavalari Borges
+// @match        https://*.feegow.com/*/*
+// @icon         https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://app.feegow.com/&size=16
+// @grant        GM_xmlhttpRequest
+// @connect      raw.githubusercontent.com
+// ==/UserScript==
