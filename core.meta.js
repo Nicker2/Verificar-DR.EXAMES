e228b7544fcf7d8a7ef7712e4e1d42566b2f5ba1
Nicolas <34519753+Nicker2@users.noreply.github.com>
2026-07-11 06:39:48 -0300

    Update core.meta.js

--- core.meta.js
+++ core.meta.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         Suite Feegow Enhanced Core Injector Agent
 // @namespace    https://github.com/Nicker2
-// @version      1.0
-// @description  Puxa o código fonte em tempo real do GitHub.
-// @author Nicolas Bonza Cavalari Borges
+// @version      1.1
+// @description  Puxa o código fonte do GitHub de forma inteligente usando Cache.
+// @author       Nicolas Bonza Cavalari Borges
 // @match        https://*.feegow.com/*/*
 // @icon         https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://app.feegow.com/&size=16
 // @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
 // @connect      raw.githubusercontent.com
 // ==/UserScript==
