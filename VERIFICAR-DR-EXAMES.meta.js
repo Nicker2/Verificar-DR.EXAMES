1cbccf7e639d792189349bd143564d089b121e0d
Nicolas <34519753+Nicker2@users.noreply.github.com>
2026-07-11 07:23:16 -0300

    Update VERIFICAR-DR-EXAMES.meta.js

--- VERIFICAR-DR-EXAMES.meta.js
+++ VERIFICAR-DR-EXAMES.meta.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name Contar DR. EXAMES com Logs Detalhados e Manter Valor 30 no Select
+// @name Suite Feegow Enhanced
 // @namespace https://github.com/Nicker2/Verificar-DR.EXAMES
-// @version 4.9.3.5
+// @version 4.9.3.6
 // @description Conta pacientes DR. EXAMES com logs detalhados, exibe apenas a lista superior por padrão, oculta a lista inferior até que a superior esteja fora de vista, nomes como hyperlinks azuis sem sublinhado, adiciona botão para alternar visibilidade, destaca "Primeira vez" com badge, intercepta dados de login e faz Bypass Invisível de sessão dupla via Fetch API com tela de carregamento, adiciona especialidade e mantém valor 30.
-// @author Você
+// @author Nicolas Bonza Cavalari Borges
 // @match https://*.feegow.com/*/*
 // @downloadURL https://raw.githubusercontent.com/Nicker2/Verificar-DR.EXAMES/refs/heads/main/VERIFICAR-DR-EXAMES.user.js
 // @updateURL https://raw.githubusercontent.com/Nicker2/Verificar-DR.EXAMES/refs/heads/main/VERIFICAR-DR-EXAMES.meta.js
