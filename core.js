80fd850a5208378098e51bb6ee6e85fe25d4f0c1
Nicolas <34519753+Nicker2@users.noreply.github.com>
2026-07-11 06:26:20 -0300

    Create core.js

---
+++ core.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Suite Feegow Enhanced Core Injector Agent
+// @namespace    https://github.com/Nicker2
+// @version      1.0
+// @description  Puxa o código fonte em tempo real do GitHub.
+// @author Nicolas Bonza Cavalari Borges
+// @match        https://*.feegow.com/*/*
+// @icon         https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://app.feegow.com/&size=16
+// @grant        GM_xmlhttpRequest
+// @connect      raw.githubusercontent.com
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    // O ?nocache gera um número aleatório toda vez, enganando o cache do GitHub
+    const url = "https://raw.githubusercontent.com/Nicker2/Verificar-DR.EXAMES/main/VERIFICAR-DR-EXAMES.user.js?nocache=" + new Date().getTime();
+
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: url,
+        onload: function(response) {
+            try {
+                // Executa o código puxado do seu GitHub
+                eval(response.responseText);
+            } catch (e) {
+                console.error("Erro ao carregar o script remoto:", e);
+            }
+        }
+    });
+})();
